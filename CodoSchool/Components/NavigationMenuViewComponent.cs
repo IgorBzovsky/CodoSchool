b6bdbfbd15b11a291e294c0491dc0918a0a6492a
Igor Bzovsky <igor77188@gmail.com>
2017-07-11 13:55:17 +0300

    Corrections_UnitOfWork_Repositories

--- CodoSchool/Components/NavigationMenuViewComponent.cs
+++ CodoSchool/Components/NavigationMenuViewComponent.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CodoSchool.Components
 {
-    public class NavigationMenuViewComponent
+    public class NavigationMenuViewComponent : ViewComponent
     {
+        public string Invoke()
+        {
+            return "Hello from navigation view component";
+        }
     }
 }
